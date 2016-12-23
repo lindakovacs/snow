9583c00092862e16d98f6e1108fff47780325780
j.akinin <j.akinin@osnovalab.ru>
2016-12-23 14:09:02 +0300

    fix build scripts for windows

--- build_win_models.cmd
+++ build_win_models.cmd
@@ -4,17 +4,17 @@ echo off
 call download_win.cmd
 if errorlevel 1 goto fail
 
-:: call build_win.cmd debug x64
-:: if errorlevel 1 goto fail
+call build_win.cmd debug x64
+if errorlevel 1 goto fail
 
-:: call build_win.cmd debug x86
-:: if errorlevel 1 goto fail
+call build_win.cmd debug x86
+if errorlevel 1 goto fail
 
-:: call build_win.cmd release x64
-:: if errorlevel 1 goto fail
+call build_win.cmd release x64
+if errorlevel 1 goto fail
 
-:: call build_win.cmd release x86
-:: if errorlevel 1 goto fail
+call build_win.cmd release x86
+if errorlevel 1 goto fail
 
 echo *********************************************
 echo BUILD MODELS SUCCEEDED
