e8064d992a702e7c8fa3e7cd6f361e5a09462347
abronix <akn.yuri@gmail.com>
2016-12-29 00:05:46 +0300

    uncomment all target for windows in build scripts

--- build_win_models.cmd
+++ build_win_models.cmd
@@ -15,14 +15,14 @@ if errorlevel 1 goto fail
 call build_win.cmd debug x64
 if errorlevel 1 goto fail
 
-:: call build_win.cmd debug x86
-:: if errorlevel 1 goto fail
+call build_win.cmd debug x86
+if errorlevel 1 goto fail
 
-:: call build_win.cmd release x64
-:: if errorlevel 1 goto fail
+call build_win.cmd release x64
+if errorlevel 1 goto fail
 
-:: call build_win.cmd release x86
-:: if errorlevel 1 goto fail
+call build_win.cmd release x86
+if errorlevel 1 goto fail
 
 echo *********************************************
 echo BUILD MODELS SUCCEEDED
