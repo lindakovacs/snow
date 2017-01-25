353b54aa7ce05faa7adf927f742255430e5e30ab
j.akinin <j.akinin@osnovalab.ru>
2017-01-25 10:22:20 +0300

    add launch script for confugure poco

--- build_win_models.cmd
+++ build_win_models.cmd
@@ -4,13 +4,16 @@ echo off
 :: Visual Studio 14 2015 [arch]
 :: Visual Studio 12 2013 [arch]
 :: Visual Studio 11 2012 [arch]
-set vc_number=14
+set vc_number=12
 
 :: set model
 :: [DEBUG|RELEASE|RELWITHDEBINFO|MINSIZEREL]
 
-:: call download_win.cmd
-:: if errorlevel 1 goto fail
+call launch_win.cmd download
+if errorlevel 1 goto fail
+
+call launch_win.cmd fixture
+if errorlevel 1 goto fail
 
 call build_win.cmd debug x64
 if errorlevel 1 goto fail
